422f600dc1fcb7cdf9f44da2ac396b70182732fe
Alberto-Vega <el_vega@live.com>
2015-11-23 12:57:29 -0800

    Initial Commit

---
+++ LocationBasedReminders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationBasedReminders
+//
+//  Created by Alberto Vega Gonzalez on 11/23/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocationBasedReminders/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LocationBasedReminders
+//
+//  Created by Alberto Vega Gonzalez on 11/23/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
